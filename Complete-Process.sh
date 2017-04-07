a8dfcbbd51ddb46ba62de69071196eacfff7a951
Mr. Joe <joe@example.com>
2017-04-07 07:11:28 +0000

    generated output json file

--- Complete-Process.sh
+++ Complete-Process.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 aws cloudformation create-stack --stack-name CreateStack-CFN --template-body file:///home/ec2-user/fd-cf/fd-cf/CreateStack-CFN.json
 
+echo "Please wait Stack Creation In Progress ... "
+
 for i in {1..30}
 do
- /bin/sleep 1
- echo $i
+ /bin/sleep 2
+ echo -ne "."
 done
 
-aws cloudformation describe-stacks --stack-name CreateStack-CFN list-exports >> ResourceDetailInStack.json
+aws cloudformation describe-stacks --stack-name CreateStack-CFN >> ResourceDetailInStack.json
 
 git add .
 
